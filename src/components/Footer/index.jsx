a05e077adf08dcbe4d03d815442a8b74e91edd3f
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-12 13:24:50 +0100

    Update layout on Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -2,18 +2,13 @@ import styled from 'styled-components'
 
 const FooterContainer = styled.footer`
   text-align: center;
-  font-weight: 500;
   font-size: 14px;
-
-  :last-child{
-    font-size: 12px;
-  }
 `;
 
-const Footer = props => (
+const Footer = () => (
   <FooterContainer>
     <p>Developed by Dedaldino3D</p>
-    <p>Copyright {new Date().getFullYear()}</p>
+    <p>&#169; Copyright {new Date().getFullYear()}</p>
   </FooterContainer>
 )
 

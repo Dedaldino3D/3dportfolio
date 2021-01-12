b648187424a46234f468cf9e7e560954643b3bd1
Dedaldino3D <dedaldinoantonio7@gmail.com>
2021-01-12 13:20:09 +0100

    Full edit on App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,77 @@
+import React, { useState, useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { HashRouter } from "react-router-dom";
+
+import Header from "../Header";
+import Home from "../Home";
+import BackTop from "../BackTop";
+import GlobalStyles from "../Styles";
+import { ButtonNav } from "../Home/styles";
+
+interface Props {
+  ref: any;
+}
+
+// const Button = React.forwardRef((props, ref) => {
+//   const [td, setTd] = useState(false);
+//   const toggle = () => {
+//     ref.current.style = "backgroud: #fff";
+//     setTd(!td);
+//   };
+
+//   return (
+//     <ButtonNav onClick={toggle} type="button" className="mobile-nav-toggle">
+//       {!td ? (
+//         <span>
+//           <FaBars />
+//         </span>
+//       ) : (
+//         <span>
+//           <FaTimes />
+//         </span>
+//       )}
+//     </ButtonNav>
+//   );
+// });
+
 function App() {
-  return <div className="App">Dedaldino Portfolio App</div>;
+  const homeRef = useRef<HTMLElement>(null);
+  const aboutRef = useRef<HTMLHeadingElement>(null);
+  const contactRef = useRef<HTMLHeadingElement>(null);
+  const portRef = useRef<HTMLHeadingElement>(null);
+  const testRef = useRef<HTMLHeadingElement>(null);
+  const servicesRef = useRef<HTMLHeadingElement>(null);
+  const skillsRef = useRef<HTMLHeadingElement>(null);
+
+  return (
+    <HashRouter>
+      <GlobalStyles />
+      {/* <Button ref={ref} /> */}
+      <Header
+        refs={{
+          aboutRef,
+          contactRef,
+          portRef,
+          testRef,
+          servicesRef,
+          skillsRef,
+          homeRef,
+        }}
+      />
+      <Home
+        refs={{
+          aboutRef,
+          contactRef,
+          portRef,
+          testRef,
+          servicesRef,
+          skillsRef,
+          homeRef,
+        }}
+      />
+      <BackTop ref={homeRef} />
+    </HashRouter>
+  );
 }
 
 export default App;
